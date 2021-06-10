5fb64f6ae6f1175434cb88d7e032c7f126241856
Fabian Cook <hello@fabiancook.dev>
2021-06-10 22:00:05 +1200

    Add size to and

--- packages/examples/src/examples/class/shared.tsx
+++ packages/examples/src/examples/class/shared.tsx
@@ -1,4 +1,5 @@
 import { h } from "../../jsx";
+import {And} from "../experiments/combinational/and";
 
 class Component {
 
@@ -6,14 +7,7 @@ class Component {
 
   async *[Symbol.asyncIterator]() {
     const id = this.iterations += 1;
-    yield `Iteration ${id}`;
-    const randomPicked = Math.random() > 0.5 ? "Task 1" : "Task 2";
-    yield `Doing task ${randomPicked}`;
-    if (randomPicked === "Task 1") {
-      await new Promise<void>(queueMicrotask);
-    } else {
-      await new Promise<void>(resolve => setTimeout(resolve, 5));
-    }
+    yield { reference: `Iteration ${id}` };
   }
 
 }
@@ -22,8 +16,10 @@ const node = <Component />
 
 export const _405_SharedInstance = (
   <container>
-    {node}
-    {node}
-    {node}
+    <And size={3} self>
+      {node}
+      {node}
+      {node}
+    </And>
   </container>
 )
--- packages/examples/src/examples/experiments/combinational/and.tsx
+++ packages/examples/src/examples/experiments/combinational/and.tsx
@@ -1,8 +1,17 @@
-import { h, Fragment, VNode } from "@virtualstate/fringe";
+import {h, Fragment, VNode, createFragment} from "@virtualstate/fringe";
 import { isTrue } from "./truth";
 import { True, False } from "./truth";
 
-export async function *And(o: unknown, state: VNode) {
+export interface AndOptions {
+  size?: number;
+  // Wraps in Truthful And node & provides children result.
+  // To be used with size
+  //
+  // This is to bind a specific sized children group into a parent
+  self?: boolean;
+}
+
+export async function *And({ size, self }: AndOptions, state: VNode) {
   // We have no child state, no input is true
   if (!state) return
   // We have a non fragment child, all input is true
@@ -13,9 +22,20 @@ export async function *And(o: unknown, state: VNode) {
   let everYielded = false;
   for await (const values of children) {
     // We have exactly the size we are after
-    const has = values.every(isTrue);
+    const has = values.every(isTrue) && (!size || values.length === size);
     if (!has && !everYielded) continue;
-    yield has ? <True /> : <False />
+    yield has ? ((self && size) ? result(values) : <True />) : <False />
     everYielded = true;
+  }
+
+  function result(values: VNode[]) {
+    return {
+      reference: Symbol("Truthful AND"),
+      children: {
+        async *[Symbol.asyncIterator]() {
+          yield values;
+        }
+      }
+    }
   }
 }
