33d0f8839eb28e8c56bf3405af26c66516960fc6
Fabian Cook <hello@fabiancook.dev>
2021-06-10 21:34:53 +1200

    Update comment

--- packages/examples/src/examples/experiments/combinational/truthful.tsx
+++ packages/examples/src/examples/experiments/combinational/truthful.tsx
@@ -2,6 +2,7 @@ import { h, VNode } from "@virtualstate/fringe";
 import { isTrue } from "./truth";
 
 // This is like an AND, but only cares about if we _finish_ with an AND
+// If it does not finish in an AND, it will throw an error.
 export async function Truthful(o: unknown, state?: VNode) {
   const children = state?.children;
   if (!children) {
