80b5fe91251f926c3397353be5762826861a48f1
Fabian Cook <hello@fabiancook.dev>
2021-06-10 23:35:24 +1200

    Shared state collection

--- packages/examples/src/examples/class/index.ts
+++ packages/examples/src/examples/class/index.ts
@@ -3,3 +3,4 @@ export * from "./options";
 export * from "./children";
 export * from "./reference";
 export * from "./shared";
+export * from "./shared-collector";
---
+++ packages/examples/src/examples/class/shared-collector.tsx
@@ -0,0 +1,44 @@
+import { h } from "../../jsx";
+import {And} from "../experiments/combinational/and";
+import {Collector} from "microtask-collector";
+
+class Component {
+
+  iterations = 0;
+
+  collector = new Collector<number>({
+    eagerCollection: true
+  });
+
+  seen = [];
+
+  constructor(public options: { expect: number }) {
+    void this.watch();
+  }
+
+  async watch() {
+    for await (const batch of this.collector) {
+      this.seen.push(...batch)
+    }
+  }
+
+  async *[Symbol.asyncIterator]() {
+    const id = this.iterations += 1;
+    this.collector.add(id);
+    yield { reference: `Iteration ${id}` };
+    yield `Seen: ${this.seen.join(", ")}`;
+  }
+
+}
+
+const node = <Component expect={3} />
+
+export const _405_SharedCollector = (
+  <container>
+    <And size={3} self>
+      {node}
+      {node}
+      {node}
+    </And>
+  </container>
+)
