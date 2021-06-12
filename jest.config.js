3930f03c7687005dfe68137970f00e6f677af0a4
Fabian Cook <hello@fabiancook.dev>
2021-06-13 00:30:10 +1200

    With config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,36 @@
+import path from 'path';
+import { lstatSync, readdirSync } from 'fs';
+
+// get listing of packages in the mono repo
+const basePath = path.resolve(path.dirname(new URL(import.meta.url).pathname), 'packages');
+const packages = readdirSync(basePath)
+  .filter(name => {
+    return lstatSync(path.join(basePath, name)).isDirectory();
+  })
+  .filter(name => name !== "deno");
+
 export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    ...packages.reduce(
+      (acc, name) => ({
+        ...acc,
+        [`@virtualstate/${name}(.*)$`]:
+          `<rootDir>/packages/./${name}/src/$1`,
+      }),
+      {},
+    ),
+  },
+  modulePathIgnorePatterns: [
+    ...packages.reduce(
+      (acc, name) => [...acc, `<rootDir>/packages/${name}/lib`, `<rootDir>/packages/${name}/node_modules`],
+      []
+    ),
+  ],
   projects: [
-    '<rootDir>/packages/union',
+    ...packages.map(
+      name => `<rootDir>/packages/${name}`
+    )
   ],
 };
---
+++ packages/x/jest.config.js
@@ -0,0 +1,35 @@
+export default {
+  coverageDirectory: "coverage",
+  coveragePathIgnorePatterns: [
+    "/node_modules/",
+    "/dist/",
+    "/lib/"
+  ],
+  coverageProvider: "v8",
+  moduleFileExtensions: [
+    "ts",
+    "tsx",
+    "js"
+  ],
+  testEnvironment: "jsdom",
+  testMatch: [
+    "**/__tests__/**/*.[jt]s?(x)",
+    "**/?(*.)+(spec|test).[tj]s?(x)"
+  ],
+  testPathIgnorePatterns: [
+    "/node_modules/",
+    "/dist/",
+    "/examples/",
+    "/lib/"
+  ],
+  transform: {
+    "^.+\\.(ts|tsx|js|jsx)?$": "ts-jest",
+    "^.+\\.js$": "babel-jest"
+  },
+  transformIgnorePatterns: [
+    // "/node_modules/",
+    // "/dist/",
+    "\\.pnp\\.[^\\/]+$"
+  ],
+  verbose: true,
+};
