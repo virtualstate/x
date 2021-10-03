d5d03a2e294cb1a16e69843ecd4b3debb828a4a7
Fabian Cook <hello@fabiancook.dev>
2021-10-03 15:32:48 +1300

    Add resolve

--- packages/fringe/src/resolve.ts
+++ packages/fringe/src/resolve.ts
@@ -1,7 +1,15 @@
 import {createToken} from "./token";
 import {EnableThen} from "./then";
 
+
+/**
+ * @experimental
+ */
 export const ResolveSymbol = Symbol("Resolve");
+
+/**
+ * @experimental
+ */
 export const Resolve = createToken(ResolveSymbol, {
   [EnableThen]: true
 });
