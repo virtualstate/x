9c9bf3b2afa5a4683d3a13da48ea8a696c68dd10
Fabian Cook <hello@fabiancook.dev>
2021-10-16 13:03:47 +1300

    Add experimental dynamic types

--- packages/typescript/src/vnode.ts
+++ packages/typescript/src/vnode.ts
@@ -1,18 +1,91 @@
-import { isSourceReference, SourceReference, MarshalledSourceReference } from "./source-reference";
-import { isAsyncIterable, AsyncIterableLike, isIterable } from "iterable";
+import { SourceReference, MarshalledSourceReference } from "./source-reference";
+import { AsyncIterableLike } from "iterable";
 import { Fragment } from "./fragment";
 import type { PromiseVNodeThenFn } from "./then";
+
+/**
+ * @experimental
+ */
+interface BaseVNodeTypeContext {
+  referenceKey: string | "reference" | "ref";
+  referenceType: SourceReference;
+  childrenKey: string | "children";
+  childrenType: AsyncIterable<unknown> | Iterable<unknown>;
+  optionsKey: string | "options" | "props";
+  optionsType: Record<string, unknown> | object;
+}
+
+/**
+ * @experimental
+ */
+interface DefaultVNodeTypeContext<R extends SourceReference = SourceReference, O extends object = object, C extends AsyncIterable<unknown> = AsyncIterable<unknown>> extends BaseVNodeTypeContext {
+  referenceKey: "reference";
+  referenceType: R;
+  optionsKey: "options";
+  optionsType: O;
+  childrenKey: "children";
+  childrenType: C;
+}
+
+/**
+ * @experimental
+ */
+type ConfiguredReferenceVNode<Context extends BaseVNodeTypeContext> = {
+  [P in Context["referenceKey"]]: Context["referenceType"]
+}
+
+/**
+ * @experimental
+ */
+type ConfiguredChildrenVNode<Context extends BaseVNodeTypeContext> = {
+  [P in Context["childrenKey"]]: Context["childrenType"]
+}
+
+/**
+ * @experimental
+ */
+type ConfiguredOptionsVNode<Context extends BaseVNodeTypeContext> = {
+  [P in Context["optionsKey"]]: Context["optionsType"]
+}
+
+/**
+ * @experimental
+ */
+type ConfiguredVNode<Context extends BaseVNodeTypeContext> =
+  & ConfiguredReferenceVNode<Context>
+  & ConfiguredChildrenVNode<Context>
+  & ConfiguredOptionsVNode<Context>;
+
+/**
+ * @experimental
+ */
+type ConfiguredVNodeUnfoldedChildren<Context extends BaseVNodeTypeContext, Node extends ConfiguredVNode<Context>> =
+  Node[Context["childrenKey"]] extends AsyncIterable<ConfiguredVNode<DefaultVNodeTypeContext>[]> ? Node[Context["childrenKey"]] :
+    Node[Context["childrenKey"]] extends Iterable<ConfiguredVNode<DefaultVNodeTypeContext>> ?
+      Node[Context["childrenKey"]] extends Iterable<infer T> ? AsyncIterable<T[]> : never :
+        Node[Context["childrenKey"]] extends Iterable<ConfiguredVNode<Context>> ?
+          Node[Context["childrenKey"]] extends Iterable<infer T> ?
+            T extends ConfiguredVNode<Context> ? AsyncIterable<ConfiguredVNodeUnfolded<Context, T>[]> : never
+            : never
+            : never;
+
+/**
+ * @experimental
+ */
+type ConfiguredVNodeUnfolded<Context extends BaseVNodeTypeContext, Node extends ConfiguredVNode<Context>> =
+  ConfiguredVNode<DefaultVNodeTypeContext<Node[Context["referenceKey"]], Node[Context["optionsKey"]], ConfiguredVNodeUnfoldedChildren<Context, Node>>>
 
 /**
  * Generic VNode, represents a virtual node within a state tree
  *
  * The VNode can be used to hydrate native state or external sources
  */
-export interface VNode {
+export interface VNode  {
   /**
    * A unique reference to this {@link VNode}, this could be a globally unique symbol like {@link Fragment}
    */
   reference: SourceReference;
+
   /**
    * An `AsyncIterable` that will return a `VNode[]` that represents a group of children updates
    * Each iteration represents an update to the {@link VNode}'s children state
